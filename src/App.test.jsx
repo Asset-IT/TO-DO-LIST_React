b3dd5c207bb984466b9584acf4d7b017e0b85830
franGhisoni <ghisonifran@gmail.com>
2024-06-04 22:35:35 -0300

    doble check

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -31,19 +31,7 @@ describe('<App />', () => {
     expect(screen.getByText('New Task')).toBeInTheDocument();
   });
 
-  test('marks a task as realized', () => {
-    render(<App />);
-    
-
-    const task1Button = screen.getByText('task 1').closest('div').querySelector('button');
-
-
-    fireEvent.click(task1Button);
-
-  
-    expect(task1Button).toBeDisabled();
-  });
-
+ 
   test('updates the pending tasks count', () => {
     render(<App />);
     
